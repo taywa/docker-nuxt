2f184b9e41f6bdb194e5f62157d1cf02a98728ca
Yves Serrano <ys@taywa.ch>
2024-02-23 08:46:59 +0100

    updated image to nuxt3 and alpine 3:19, removed old stuff

---
+++ taskfile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Enable shell strict mode
+set -euo pipefail
+
+NUXT_VERSION="3.x"
+
+build-arch() {
+    cd docker
+	DOCKER_BUILDKIT=1 docker buildx build \
+		--load \
+		--platform linux/`arch|sed 's/x86_64/amd64/'` \
+		--build-arg BUILDKIT_INLINE_CACHE=1 \
+		-t taywa/nuxt:$NUXT_VERSION-`arch|sed 's/x86_64/amd64/'` \
+		nuxt
+}
+
+build-archs() {
+    cd docker
+	DOCKER_BUILDKIT=1 docker buildx build \
+		--push \
+		--platform linux/arm64,linux/amd64 \
+		--build-arg BUILDKIT_INLINE_CACHE=1 \
+		-t taywa/nuxt:$NUXT_VERSION-`arch|sed 's/x86_64/amd64/'` \
+		nuxt
+
+}
+
+# execute passed string as function
+"$@"
